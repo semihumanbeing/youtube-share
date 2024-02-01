7d2f132902468b3f153489e06077abebab32f614
semihumanbeing <hac.rdh96@gmail.com>
2024-02-01 17:58:24 +0900

    [Feature] Create Chatroom Page

---
+++ src/components/pages/CreateChatroomPage.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./CreateChatroomPage.css";
+
+const CreateChatroomPage = () => {
+  const [chatroomName, setChatroomName] = useState<string>("");
+  const [chatroomPassword, setChatroomPassword] = useState<string>("");
+  const [hasPwd, setHasPwd] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleCreateChatroom = async () => {
+    if (!chatroomName) {
+      setError("name cannot be empty.");
+      return;
+    }
+
+    await fetch(`${process.env.REACT_APP_BASE_URL}/chatroom`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chatroomName: chatroomName,
+        chatroomPassword: chatroomPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (!response.data) {
+          console.log(response);
+          setError(`${response.errorMsg}`);
+        }
+
+        navigate(`/chatroom/${response.data.chatroomId}`);
+      })
+      .catch((error: any) => {
+        console.log(error);
+        // setError(`error creating chatroom: ${response.errorMsg}`);
+        return;
+      });
+  };
+
+  return (
+    <div className="create-chatroom-container">
+      <h2>Create Chatroom</h2>
+      <button
+        className="haspwd-button"
+        onClick={() => {
+          setHasPwd(!hasPwd);
+          setChatroomPassword("");
+        }}
+      >
+        Private
+      </button>
+      {error && <p className="error-message">{error}</p>}
+      <label>
+        Chatroom Name
+        <input
+          type="text"
+          value={chatroomName}
+          onChange={(e) => setChatroomName(e.target.value)}
+        />
+      </label>
+
+      {hasPwd && (
+        <label>
+          Password
+          <input
+            type="password"
+            value={chatroomPassword}
+            onChange={(e) => setChatroomPassword(e.target.value)}
+          />
+        </label>
+      )}
+      <button className="create-button" onClick={handleCreateChatroom}>
+        Create
+      </button>
+    </div>
+  );
+};
+
+export default CreateChatroomPage;
